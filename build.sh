689de9514d71d1010ec89eea235459ea19a6ff67
danielclough <danielclough@gmail.com>
2020-12-09 17:26:39 +0100

    more options

--- build.sh
+++ build.sh
@@ -1,39 +1,77 @@
 #!/bin/bash
 
-# info
-
 BASE_DIR=$(dirname $(realpath $0 ))
 moreBuilder=${BASE_DIR}/moreBuilder
-
-defaultBranch=v2.13.2.7
-defaultUsername=blackcoinnl
-read -p "Are you ${defaultUsername}? (default yes, type your username to change): " USERNAME
-USERNAME=${USERNAME:-${defaultUsername}}
-if [ $USERNAME != $defaultUsername ]; then
-	sed -i "s/defaultUsername=blackcoinnl/defaultUsername=$USERNAME/" $0
-fi
-echo ${USERNAME}
 SYSTYPE=`lscpu | head -1 | tr -s ' ' | cut -d ' ' -f2`
-echo $SYSTYPE
-read -p "What version? (default $defaultBranch)" BRANCH
+
+# DockerHub Account
+
+defaultDockerHub=blackcoinnl
+read -p "What is your DockerHub Account Name? (default $defaultDockerHub): " DockerHub
+DockerHub=${DockerHub:-${defaultDockerHub}}
+if [ $DockerHub != $defaultDockerHub ]; then
+	sed -i "s/defaultDockerHub=blackcoinnl/defaultDockerHub=$DockerHub/" $0
+fi
+
+# Git Account
+
+defaultHubLab=github
+read -p "Github or Gitlab? (default $defaultHubLab): " HubLab
+HubLab=${HubLab:-${defaultHubLab}}
+if [ $HubLab != $defaultHubLab ]; then
+	sed -i "s|defaultHubLab=github|defaultGit=$HubLab|" $0
+	sed -i "s|github|$HubLab|" ${BASE_DIR}/Dockerfile.ubase
+fi
+
+defaultGit=CoinBlack
+read -p "What is your Git account? (default $defaultGit): " Git
+Git=${Git:-${defaultGit}}
+if [ $Git != $defaultGit ]; then
+	sed -i "s|defaultGit=CoinBlack|defaultGit=$Git|" $0
+	sed -i "s|CoinBlack|$Git|" ${BASE_DIR}/Dockerfile.ubase
+fi
+
+# Git Branch
+
+read -p "What branch/version? (default $defaultBranch): " BRANCH
+defaultBranch=master
 BRANCH=${BRANCH:-${defaultBranch}}
 if [ $BRANCH != $defaultBranch ]; then
-	sed -i "s/defaultBranch=v2.13.2.7/defaultBranch=$BRANCH/" $0
+	sed -i "s|defaultBranch=master|defaultBranch=$BRANCH|" $0
+	sed -i "s|ENV BRANCH=v2.13.2.7|ENV BRANCH=$BRANCH|" ${BASE_DIR}/Dockerfile.ubase
 fi
 
-# change names for multi-stage build
-
-defaultUbase=blackcoinnl/blackmore-ubase-x86_64:v2.13.2.7
-ubase="${USERNAME}/blackmore-ubase-$SYSTYPE:$BRANCH"
+# change branch for multi-stage build
+defaultUbase=blackcoinnl/blackmore-ubase-x86_64:master
+ubase="${DockerHub}/blackmore-ubase-$SYSTYPE:$BRANCH"
 if [ $defaultUbase != $ubase ]; then
-	cat ${moreBuilder}/Dockerfile.ubuntu
 	sed -i "s|FROM $defaultUbase as build|FROM $ubase as build|" ${BASE_DIR}/Dockerfile.ubuntu
-	sed -i "s|FROM $defaultUbase as build|FROM $ubase as build|" ${BASE_DIR}/Dockerfile.minimal
-	sed -i "s|defaultUbase=blackcoinnl/blackmore-ubase-x86_64:v2.13.2.7|defaultUbase=$ubase|" $0
+	sed -i "s|defaultUbase=blackcoinnl/blackmore-ubase-x86_64:master|defaultUbase=$ubase|" $0
 fi
 
-ubuntu="${USERNAME}/blackcoin-more-ubuntu-$SYSTYPE:$BRANCH"
-minimal="${USERNAME}/blackcoin-more-minimal-$SYSTYPE:$BRANCH"
+# timezone
+defaultTimezone=America/Los_Angeles
+read -p "What is your timezone? (default $defaultTimezone): " timezone
+timezone=${timezone:-${defaultTimezone}}
+if [ $timezone != $defaultTimezone ]; then
+	sed -i "s|defaultTimezone=America/Los_Angeles|defaultTimezone=$timezone|" $0
+	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubuntu
+fi
+
+
+echo "DockerHub Account: ${DockerHub}"
+echo "Git Account: $Git"
+echo $BRANCH
+echo $SYSTYPE
+echo $timezone
+
+
+
+
+minimal="${DockerHub}/blackcoin-more-minimal-$SYSTYPE:$BRANCH"
+ubuntu="${DockerHub}/blackcoin-more-ubuntu-$SYSTYPE:$BRANCH"
+ubase="${DockerHub}/blackmore-ubase-$SYSTYPE:$BRANCH"
 
 # build
 
