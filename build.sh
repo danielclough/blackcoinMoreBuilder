92ca5d1787bbd9cf80f9887db7b1e67610551503
danielclough <danielclough@gmail.com>
2020-12-25 20:42:39 -0800

    add multi-arch support

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname $(realpath $0 ))
-moreBuilder=${BASE_DIR}/moreBuilder
-SYSTYPE=`lscpu | head -1 | tr -s ' ' | cut -d ' ' -f2`
+
+# Architecture
+
+Alist="\n
+aarch64-linux-gnu, \n
+arm-linux-gnueabihf, \n
+x86_64-linux-gnu"
+
+defaultARCH=x86_64-linux-gnu
+echo -e ${Alist}
+read -p "For what architecture would you like to build? ($defaultARCH): " ARCH
+ARCH=${ARCH:-${defaultARCH}}
+if [ $DockerHub != $defaultDockerHub ]; then
+	sed -i "s/defaultARCH=x86_64-linux-gnu/defaultARCH=$ARCH/" $0
+fi
 
 # DockerHub Account
 
 defaultDockerHub=blackcoinnl
-read -p "What is your DockerHub Account Name? (default $defaultDockerHub): " DockerHub
+read -p "What is your DockerHub Account Name? ($defaultDockerHub): " DockerHub
 DockerHub=${DockerHub:-${defaultDockerHub}}
 if [ $DockerHub != $defaultDockerHub ]; then
 	sed -i "s/defaultDockerHub=blackcoinnl/defaultDockerHub=$DockerHub/" $0
@@ -16,7 +29,7 @@ fi
 # Git Account
 
 defaultHubLab=github
-read -p "Github or Gitlab? (default $defaultHubLab): " HubLab
+read -p "Github or Gitlab? ($defaultHubLab): " HubLab
 HubLab=${HubLab:-${defaultHubLab}}
 if [ $HubLab != $defaultHubLab ]; then
 	sed -i "s|defaultHubLab=github|defaultGit=$HubLab|" $0
@@ -24,68 +37,62 @@ if [ $HubLab != $defaultHubLab ]; then
 fi
 
 defaultGit=CoinBlack
-read -p "What is your Git account? (default $defaultGit): " Git
+read -p "What is your Git account? ($defaultGit): " Git
 Git=${Git:-${defaultGit}}
-if [ $Git != $defaultGit ]; then
+if [ ${Git} != ${defaultGit} ]; then
 	sed -i "s|defaultGit=CoinBlack|defaultGit=$Git|" $0
 	sed -i "s|CoinBlack|$Git|" ${BASE_DIR}/Dockerfile.ubase
 fi
 
 # Git Branch
 
-read -p "What branch/version? (default $defaultBranch): " BRANCH
 defaultBranch=master
+read -p "What branch/version? ($defaultBranch): " BRANCH
 BRANCH=${BRANCH:-${defaultBranch}}
-if [ $BRANCH != $defaultBranch ]; then
+if [ ${BRANCH} != ${defaultBranch} ]; then
 	sed -i "s|defaultBranch=master|defaultBranch=$BRANCH|" $0
 	sed -i "s|ENV BRANCH=v2.13.2.7|ENV BRANCH=$BRANCH|" ${BASE_DIR}/Dockerfile.ubase
 fi
 
-# change branch for multi-stage build
-defaultUbase=blackcoinnl/blackmore-ubase-x86_64:master
-ubase="${DockerHub}/blackmore-ubase-$SYSTYPE:$BRANCH"
-if [ $defaultUbase != $ubase ]; then
-	sed -i "s|FROM $defaultUbase as build|FROM $ubase as build|" ${BASE_DIR}/Dockerfile.ubuntu
-	sed -i "s|defaultUbase=blackcoinnl/blackmore-ubase-x86_64:master|defaultUbase=$ubase|" $0
-fi
-
 # timezone
 defaultTimezone=America/Los_Angeles
-read -p "What is your timezone? (default $defaultTimezone): " timezone
+read -p "What is your timezone? (${defaultTimezone}): " timezone
 timezone=${timezone:-${defaultTimezone}}
-if [ $timezone != $defaultTimezone ]; then
+if [ ${timezone} != ${defaultTimezone} ]; then
 	sed -i "s|defaultTimezone=America/Los_Angeles|defaultTimezone=$timezone|" $0
 	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubase
 	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubuntu
 fi
 
 
+echo "Architecture: ${ARCH}"
 echo "DockerHub Account: ${DockerHub}"
-echo "Git Account: $Git"
-echo $BRANCH
-echo $SYSTYPE
-echo $timezone
+echo "Git Account: ${Git}"
+echo ${BRANCH}
+echo ${timezone}
 
+# build ubase
 
+# set current
+Dockerfile="${BASE_DIR}/Dockerfile.${base}"
+docker build -t ubase-base --network=host - < ${Dockerfile} 
 
+# build ubase
+ubase="ubase-${ARCH}"
+Dockerfile="${BASE_DIR}/${ARCH}/Dockerfile.${ubase}"
+docker build ./${ARCH} -t ${ubase} --network=host -f ${Dockerfile}
 
-minimal="${DockerHub}/blackcoin-more-minimal-$SYSTYPE:$BRANCH"
-ubuntu="${DockerHub}/blackcoin-more-ubuntu-$SYSTYPE:$BRANCH"
-ubase="${DockerHub}/blackmore-ubase-$SYSTYPE:$BRANCH"
+# build ubuntu
+ubuntu="ubuntu-${ARCH}"
+Dockerfile="${BASE_DIR}/${ARCH}/Dockerfile.${ubuntu}"
+docker build -t ${ubuntu} - --network=host < ${Dockerfile}
+docker image tag ${ubuntu} ${DockerHub}/blackcoin-more-ubuntu-${ARCH}:latest
 
-# build
-
-docker build -t $ubase - --network=host < ${BASE_DIR}/Dockerfile.ubase
-
-docker run -itd  --network=host --name ubase $ubase bash
-
-docker cp ubase:/parts $moreBuilder
-cd $moreBuilder
-tar -C parts -c . | docker import - $minimal
-
-docker container rm -f ubase
-
-# push to docker hub
-
-docker image push $minimal
-docker image push $ubuntu
+# build minimal
+minimal="minimal-${ARCH}"
+docker run -itd --network=host --name ${ubase} ${ubase} bash
+docker cp ${ubase}:${ARCH}/parts ${ARCH}/parts
+cd ${BASE_DIR}/${ARCH}
+tar -C parts -c . | docker import - ${minimal}
+docker container rm -f ${ubase}
+docker tag ${minimal} ${DockerHub}/blackcoin-more-minimal-${ARCH}:latest
