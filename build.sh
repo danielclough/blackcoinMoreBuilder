035d94b1426da252f43a7ee955053d2a0a79cc9b
danielclough <danielclough@gmail.com>
2020-12-26 10:31:04 +0100

    download and install dependencies

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@
 
 BASE_DIR=$(dirname $(realpath $0 ))
 Alist="\n
-\t \t \t aarch64-linux-gnu \n
-\t \t \t arm-linux-gnueabihf \n
-\t \t \t x86_64-linux-gnu \n
+\t \t \t \t aarch64-linux-gnu \n
+\t \t \t \t arm-linux-gnueabihf \n
+\t \t \t \t x86_64-linux-gnu \n
 "
 
 usage="Usage: \n \t 
@@ -40,59 +40,89 @@ case $1 in
 
 # Architecture
 
-defaultARCH=${${2}:-x86_64-linux-gnu}
+defaultARCH=${2:-x86_64-linux-gnu}
 echo -e ${Alist}
 read -p "For what architecture would you like to build? ($defaultARCH): " architecture
 architecture=${architecture:-${defaultARCH}}
-if [ $DockerHub != $defaultDockerHub ]; then
-	sed -i "s/defaultARCH=x86_64-linux-gnu/defaultARCH=$architecture/" $0
+if [ ${architecture} != ${defaultARCH} ]; then
+	sed -i "s/-x86_64-linux-gnu/-${architecture}/" $0
 fi
+
+
+if [[ -f ${BASE_DIR}/depends-${architecture}.tar.xz ]]; then
+	echo "Dependencies exist!" 
+else
+	wget -O ${BASE_DIR}/depends-${architecture}.tar.xz https://admin.blackcoin.nl/static/depends-${architecture}.tar.xz > /dev/null
+
+	case $architecture in
+	aarch64-linux-gnu)
+		checkSUM=`echo "f6f1b099d876db90396d0d56eb5b3f366f14c90e077524e2b10bfdaaa1aa5805 ${BASE_DIR}/depends-${architecture}.tar.xz" | sha256sum --check | awk '{print $2}' 2> /dev/null`
+		[[ "${checkSUM}" != "OK" ]] && echo 'SHASUM FAIL' && exit 1
+		tar xJf ${BASE_DIR}/depends-${architecture}.tar.xz -C ${BASE_DIR}
+	;;
+	arm-linux-gnueabihf)
+		checkSUM=`echo "339c1159adcccecb45155b316f1f5772009b92acb8cfed29464dd7f09775fb79 ${BASE_DIR}/depends-${architecture}.tar.xz" | sha256sum --check | awk '{print $2}' 2> /dev/null`
+		[[ "${checkSUM}" != "OK" ]] && echo 'SHASUM FAIL' && exit 1
+		tar xJf ${BASE_DIR}/depends-${architecture}.tar.xz -C ${BASE_DIR}
+	;;
+	x86_64-linux-gnu)
+		checkSUM=`echo "eed063b26f4c4e0fa35dc085fe09bafd4251cffa76cdabb26bf43077da03b84e ${BASE_DIR}/depends-${architecture}.tar.xz" | sha256sum --check | awk '{print $2}' 2> /dev/null`
+		[[ "${checkSUM}" != "OK" ]] && echo 'SHASUM FAIL' && exit 1
+		tar xJf ${BASE_DIR}/depends-${architecture}.tar.xz -C ${BASE_DIR}
+	;;
+	esac
+	echo -e "
+	Dependencies SHA256SUM GOOD!
+	"
+fi
+
+
 
 # DockerHub Account
 
-defaultDockerHub=${${3}:-blackcoinnl}
+defaultDockerHub=${3:-blackcoinnl}
 read -p "What is your DockerHub Account Name? ($defaultDockerHub): " DockerHub
 DockerHub=${DockerHub:-${defaultDockerHub}}
 if [ $DockerHub != $defaultDockerHub ]; then
-	sed -i "s/defaultDockerHub=blackcoinnl/defaultDockerHub=$DockerHub/" $0
+	sed -i "s/blackcoinnl/$DockerHub/" $0
 fi
 
 # Git Account
 
-defaultHubLab=${${4}:-github}
+defaultHubLab=${4:-github}
 read -p "Github or Gitlab? ($defaultHubLab): " HubLab
 HubLab=${HubLab:-${defaultHubLab}}
 if [ $HubLab != $defaultHubLab ]; then
-	sed -i "s|defaultHubLab=github|defaultHubLab=$HubLab|" $0
 	sed -i "s|github|$HubLab|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|github|$HubLab|" $0
 fi
 
-defaultRepo=${${5}:-CoinBlack}
+defaultRepo=${5:-CoinBlack}
 read -p "What is your repository name? ($defaultRepo): " repository
 repository=${repository:-${defaultRepo}}
 if [ ${repository} != ${defaultRepo} ]; then
-	sed -i "s|defaultRepo=CoinBlack|defaultRepo=$repository|" $0
 	sed -i "s|CoinBlack|$repository|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|CoinBlack|$repository|" $0
 fi
 
 # branch
 
-defaultBranch=${${6}:-master}
+defaultBranch=${6:-v2.13.2.7}
 read -p "What branch/version? ($defaultBranch): " branch
 branch=${branch:-${defaultBranch}}
 if [ ${branch} != ${defaultBranch} ]; then
-	sed -i "s|defaultBranch=master|defaultBranch=$branch|" $0
-	sed -i "s|ENV branch=v2.13.2.7|ENV branch=$branch|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|ENV branch=v2.13.2.7|ENV branch=${branch}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|v2.13.2.7|${branch}|" $0
 fi
 
 # timezone
-defaultTimezone=${${7}:-America/Los_Angeles}
+defaultTimezone=${7:-America/Los_Angeles}
 read -p "What is your timezone? (${defaultTimezone}): " timezone
 timezone=${timezone:-${defaultTimezone}}
 if [ ${timezone} != ${defaultTimezone} ]; then
-	sed -i "s|defaultTimezone=America/Los_Angeles|defaultTimezone=$timezone|" $0
-	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubase
-	sed -i "s|defaultTimezone=America/Los_Angeles|$timezone|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|America/Los_Angeles|${timezone}|" ${BASE_DIR}/Dockerfile.ubase
+	sed -i "s|America/Los_Angeles|${timezone}|" ${BASE_DIR}/Dockerfile.ubuntu
+	sed -i "s|America/Los_Angeles|${timezone}|" $0
 fi
 
 
@@ -101,10 +131,8 @@ echo "DockerHub Account: ${DockerHub}"
 echo "repository Account: ${HubLab}.com/${repository} ${branch}"
 echo ${timezone}
 
-# build ubase
-
-# set current
-Dockerfile="${BASE_DIR}/Dockerfile.${base}"
+# build ubase-base
+Dockerfile="${BASE_DIR}/Dockerfile.ubase-base"
 docker build -t ubase-base --network=host - < ${Dockerfile} 
 
 # build ubase
